b6fc71ac49af30a6e57bef729646201361a85001
MarcelaV <m.vilchespino@gmail.com>
2017-12-14 17:08:17 -0300

    Subiendo lyft

---
+++ assets/js/lyft.js
@@ -0,0 +1,10 @@
+// Función para que desaparezca imagen a los 2 segundos
+$(function(){
+	$('header').fadeOut(1000);
+});
+
+// $(function(){
+//    setTimeout(function() {
+//       $('#splash').fadeOut(500);
+//    }, 2000);
+// });
